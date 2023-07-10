b9a5903881de0380469f6611429e78867055ea59
Rodrigar <rodriadriangarcia1997@gmail.com>
2023-07-09 22:24:54 -0300

    add : modelos registrados

--- proyecto/apps/CORE/admin.py
+++ proyecto/apps/CORE/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import  models
 
+admin.site.register(models.Estudiante)
+admin.site.register(models.Curso)
+admin.site.register(models.Profesor)
 # Register your models here.
