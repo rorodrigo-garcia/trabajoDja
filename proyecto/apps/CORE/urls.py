34fbb7a67d949ec040c1d0db63d3da9f8aa53070
Rodrigar <rodriadriangarcia1997@gmail.com>
2023-07-20 11:46:27 -0300

    5

--- proyecto/apps/CORE/urls.py
+++ proyecto/apps/CORE/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index,crearEstudiante
+from .views import index,crearEstudiante,crearProfesor,crearCurso
 
 app_name='CORE'
 urlpatterns = [
     path('',index,name="index"),
-    path('crearEstudiante',crearEstudiante,name="crearEstudiante")
+    path('crearEstudiante/',crearEstudiante,name="crearEstudiante"),
+    path('crearProfesor/',crearProfesor,name="crearProfesor"),
+    path('crearCurso/',crearCurso,name="crearCurso")
 ]
--- proyecto/apps/CORE/views.py
+++ proyecto/apps/CORE/views.py
@@ -19,3 +19,21 @@ def crearEstudiante(request: HttpRequest) -> HttpResponse:
     else:
         form = EstudianteForm()
     return render(request,"CORE/crearEstudiante.html",{"form":form})
+
+def crearProfesor(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form =ProfesorForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProfesorForm()
+    return render(request,"CORE/crearProfesor.html",{"form":form})
+
+def crearCurso(request: HttpRequest)-> HttpResponse:
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CursoForm()
+    return render(request,"CORE/crearCurso.html" ,{"form":form})
